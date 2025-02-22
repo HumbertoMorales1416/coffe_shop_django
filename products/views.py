bdcaf5fa304269ad7bd12d9f39aa1c05112a1971
HumbertoMorales1416 <humberto.morales.14@hotmail.com>
2025-02-22 13:59:57 -0800

    Begin add scss styles

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from products.forms import ProductForm
+from products.models import Product
 
 class ProductFormView(generic.FormView):
   template_name = 'products/add_product.html'
@@ -10,3 +11,11 @@ class ProductFormView(generic.FormView):
   def form_valid(self, form):
     form.save()
     return super().form_invalid(form)
+  
+class ProductListView(generic.ListView):
+  model: Product
+  template_name = 'products/list_products.html'
+  context_object_name = 'products'
+  
+  def get_queryset(self):
+    return Product.objects.order_by('id')
